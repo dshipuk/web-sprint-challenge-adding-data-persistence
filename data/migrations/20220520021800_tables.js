d59e86495abbf365aed80a2b1b323c377d096a74
unknown <dshipuk@yahoo.com>
2022-05-19 19:54:10 -0700

    nothing major just formatting

--- data/migrations/20220520021800_tables.js
+++ data/migrations/20220520021800_tables.js
@@ -1,36 +1,37 @@
 exports.up = async function (knex) {
     await knex.schema.createTable("projects", (table) => {
-        table.increments("project_id")
-        table.string("project_name").notNullable()
-        table.string("project_description")
-        table.bool("project_completed")
+      table.increments("project_id");
+      table.string("project_name", 128).notNullable();
+      table.string("project_description", 128);
+      table.bool("project_completed");
     });
   
     await knex.schema.createTable("resources", (table) => {
-        table.increments("resource_id")
-        table.string("resource_name").unique()
-        table.string("resource_description")
+      table.increments("resource_id");
+      table.string("resource_name", 128).unique().notNullable();
+      table.string("resource_description", 128);
     });
   
     await knex.schema.createTable("tasks", (table) => {
-        table.increments("task_id")
-        table.string("task_description").notNullable()
-        table.string("task_notes")
-        table.bool("task_completed")
-        table.integer("project_id")
-            .unsigned()
-            .notNullable()
-            .references("project_id")
-            .inTable("projects")
-            .onDelete("RESTRICT")
-            .onUpdate("RESTRICT")
+      table.increments("task_id");
+      table.string("task_description", 128).notNullable();
+      table.string("task_notes", 128);
+      table.bool("task_completed");
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
     });
   };
   
   exports.down = async function (knex) {
-      await knex.schema
-        .dropTableIfExists("tasks")
-        .dropTableIfExists("resources")
-        .dropTableIfExists("projects")
+    await knex.schema
+      .dropTableIfExists("tasks")
+      .dropTableIfExists("resources")
+      .dropTableIfExists("projects");
   };
   
